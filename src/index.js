8f8af919accdad0fe2608b814aae6abe7a75ce21
ojas-17 <ojasbalsaraf@gmail.com>
2024-10-10 22:03:35 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(precess.env.PORT || 8000, () => {
+        console.log(`MONGO DB connected on port: ${precess.env.PORT || 8000}`);
+    });
+})
+.catch((error) => {
+    console.log("MONGO DB connection failed", error);
+});
